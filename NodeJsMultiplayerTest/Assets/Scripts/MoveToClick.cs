83b1f5871f26587f0ed230dab5b5f0d06888c2da
astonished12 <dan_cehan@yahoo.com>
2017-03-03 10:06:07 +0200

    add player script navigator ONE CLASS ONE RESPONSABILITY

--- NodeJsMultiplayerTest/Assets/Scripts/MoveToClick.cs
+++ NodeJsMultiplayerTest/Assets/Scripts/MoveToClick.cs
@@ -7,36 +7,13 @@ public class MoveToClick : MonoBehaviour {
 
     public GameObject player;
     private List<Node> path;
-    int targetIndex;
-
-
+    
     public void OnClickPressed(Vector3 targetPosition)
     {
-        targetIndex = 0;
         var pathfinder = GetComponent<PathFinder>();
         path = pathfinder.AStar(player.transform.position, targetPosition);
-        StopCoroutine("FollowPath");
-        StartCoroutine("FollowPath");
-    }
-    IEnumerator FollowPath()
-    {
-        Vector3 currentWaypoint = path[0].worldPosition;
-        while (true)
-        {
-            if (player.transform.position == currentWaypoint)
-            {
-                targetIndex++;
-                if (targetIndex >= path.Count-1)
-                {
-                    yield break;
-                }
-                currentWaypoint = path[targetIndex].worldPosition;
-            }
-
-            player.transform.position = Vector3.MoveTowards(player.transform.position, currentWaypoint, 50 * Time.deltaTime);
-            yield return null;
-
-        }
-    }
+        var navigator = player.GetComponent<NavagiateToPosition>();
+        navigator.SetDestination(path);       
+    }    
 
 }
--- NodeJsMultiplayerTest/Assets/Scripts/NavagiateToPosition.cs
+++ NodeJsMultiplayerTest/Assets/Scripts/NavagiateToPosition.cs
@@ -1,16 +1,41 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NavagiateToPosition : MonoBehaviour {
+public class NavagiateToPosition : MonoBehaviour
+{
+    int targetIndex;
+    private List<Node> path;
+    public float speed = 20f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void SetDestination(List<Node> _path)
+    {
+        path = _path;
+        targetIndex = 0;
+        StopCoroutine("FollowPath");
+        StartCoroutine("FollowPath");
+    }
+    IEnumerator FollowPath()
+    {
+        Vector3 currentWaypoint = path[0].worldPosition;
+        while (true)
+        {
+            if (transform.position == currentWaypoint)
+            {
+                targetIndex++;
+                if (targetIndex >= path.Count - 1)
+                {
+                    yield break;
+                }
+                currentWaypoint = path[targetIndex].worldPosition;
+            }
+
+            transform.position = Vector3.MoveTowards(transform.position, currentWaypoint, speed * Time.deltaTime);
+            yield return null;
+
+        }
+    }
+
+    
 }
