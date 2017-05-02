4521f47786adbf2db2690a9f4bd872cc6523824d
astonished12 <dan_cehan@yahoo.com>
2017-05-02 17:19:45 +0300

    working on skills@

--- NodeJsMultiplayerTest/Assets/Scripts/AI/SpecialAttack.cs
+++ NodeJsMultiplayerTest/Assets/Scripts/AI/SpecialAttack.cs
@@ -9,6 +9,7 @@ public class SpecialAttack : MonoBehaviour {
     public float damage;
     public bool inAction;
     public Texture2D pictureSkill;
+    public GameObject fireBall;
     // Use this for initialization
     void Start () { 
 	    	
@@ -16,10 +17,24 @@ public class SpecialAttack : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetKeyDown(key))
+        if (Input.GetKeyDown(key) && key.ToString()=="Q")
         {
             inAction = true;
             Debug.Log("Special attack ON " + key.ToString());
+            GameObject g = Instantiate(fireBall, GameObject.FindGameObjectWithTag("Player").transform.position, Quaternion.identity) as GameObject;
+            /*float forceDirX, forceDirY, forceDirZ;
+            forceDirX = Random.Range(-10, 10);
+            forceDirY = Random.Range(5, 15);
+            forceDirZ = Random.Range(-10, 10);*/
+
+            Vector3 sp = Camera.main.WorldToScreenPoint(transform.position);
+            Vector3 dir = (Input.mousePosition - sp).normalized;
+            g.GetComponent<Rigidbody>().AddForce(dir * 5);
+
+            //g.GetComponent<Rigidbody>().AddForce(new Vector2(wp.x, wp.y), 0);
+
+            //g.GetComponent<Rigidbody>().AddForce(forceDirX * 100, forceDirY * 100, forceDirZ * 100);
+            Destroy(g, 5);
         }
 	}
 }
