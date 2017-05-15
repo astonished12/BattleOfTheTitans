bbcc83eca8ac5e193044d170a3fe4cef1f2c6ae4
astonished12 <dan_cehan@yahoo.com>
2017-05-15 22:19:46 +0300

    add messageboxes

--- NodeJsMultiplayerTest/Assets/Scripts/StartGame/Register.cs
+++ NodeJsMultiplayerTest/Assets/Scripts/StartGame/Register.cs
@@ -1,26 +1,128 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
+using System.Text.RegularExpressions;
+using UnityEngine.UI;
+using Main.Assets.Scripts;
 
 public class Register : MonoBehaviour {
 
-    public GameObject username;
-    public GameObject email;
-    public GameObject password;
+    public GameObject usernameField;
+    public GameObject emailField;
+    public GameObject passwordField;
 
     private string Username;
     private string Email;
     private string Password;
+    private string form;
+    private bool EmailValid = false;
+
+
 
     public void ChangeLevelToLogin()
     {
         SceneManager.LoadScene(0);
     }
 
-   // public void 
-    public void RegisterUser()
+    public void SendDataToServer()
     {
 
+    }
+    public void RegisterUser()
+    {
+        if (ValidationInputs())
+            Debug.Log("GOOD REGISTER");
+        else
+        {
+            var messageBox = Helpers.BringMessageBox();
+            messageBox.transform.position = passwordField.transform.position;
+            messageBox.SetMessage("Pass wrong");
+            
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (usernameField.GetComponent<InputField>().isFocused)
+            {
+                passwordField.GetComponent<InputField>().Select();
+            }
+            if (passwordField.GetComponent<InputField>().isFocused)
+            {
+                emailField.GetComponent<InputField>().Select();
+            }
+            if (emailField.GetComponent<InputField>().isFocused)
+            {
+                usernameField.GetComponent<InputField>().Select();
+            }
+           
+        }
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            RegisterUser();
+        }
+
+        Username = usernameField.GetComponent<InputField>().text;
+        Email = emailField.GetComponent<InputField>().text;
+        Password = passwordField.GetComponent<InputField>().text;
+
+    }
+
+    private bool ValidationInputs()
+    {
+        if (Password != "" && Email != "" && Password != "")        
+            return true;        
+        else
+            return false;
+    }
+
+    private bool ValidatePassword(string password, out string ErrorMessage)
+    {
+        var input = password;
+        ErrorMessage = string.Empty;
+
+        if (String.IsNullOrEmpty(input))
+        {
+            throw new Exception("Password should not be empty");
+        }
+
+        var hasNumber = new Regex(@"[0-9]+");
+        var hasUpperChar = new Regex(@"[A-Z]+");
+        var hasMiniMaxChars = new Regex(@".{8,15}");
+        var hasLowerChar = new Regex(@"[a-z]+");
+        var hasSymbols = new Regex(@"[!@#$%^&*()_+=\[{\]};:<>|./?,-]");
+
+        if (!hasLowerChar.IsMatch(input))
+        {
+            ErrorMessage = "Password should contain At least one lower case letter";
+            return false;
+        }
+        else if (!hasUpperChar.IsMatch(input))
+        {
+            ErrorMessage = "Password should contain At least one upper case letter";
+            return false;
+        }
+        else if (!hasMiniMaxChars.IsMatch(input))
+        {
+            ErrorMessage = "Password should not be less than or greater than 12 characters";
+            return false;
+        }
+        else if (!hasNumber.IsMatch(input))
+        {
+            ErrorMessage = "Password should contain At least one numeric value";
+            return false;
+        }
+
+        else if (!hasSymbols.IsMatch(input))
+        {
+            ErrorMessage = "Password should contain At least one special case characters";
+            return false;
+        }
+        else
+        {
+            return true;
+        }
     }
 }
